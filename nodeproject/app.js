f9966205c4e8a402bc77ebd4747316915cdc7665
EC2 Default User <ec2-user@ip-172-31-3-94.us-east-2.compute.internal>
2018-12-11 15:24:04 +0000

    node project cleaned

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,21 @@
 var hello = 'hello world!!!';
 console.log(hello);
+
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Reponse sent by node backend");
+})
+
+app.get("/routing", (req, res) => {
+    res.send("Reponse of express routing");
+})
+
+
+app.listen(port, function(err){
+    console.log("The server is running on port " + port);
+})
+
